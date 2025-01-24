4ecf1725af1b0663a0478aa235d8ba326c5170ba
catalyst <wealthralph@gmail.com>
2025-01-24 14:30:23 +0100

    updated the base url

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://visual-ict-script.onrender.com",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://visual-ict-script.onrender.com",
 });
